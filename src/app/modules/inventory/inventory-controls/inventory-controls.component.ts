ab234faabafa5a7820518873fcbad48355e990a1
Cristian <cristianarangodaza@gmail.com>
2020-09-19 17:23:55 -0500

    Inventory Component

---
+++ src/app/modules/inventory/inventory-controls/inventory-controls.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-controls',
+  templateUrl: 'inventory-controls.component.html',
+  styleUrls: ['./inventory-controls.component.scss'],
+})
+export class InventoryControlsComponent {}
--- src/app/modules/inventory/inventory.module.ts
+++ src/app/modules/inventory/inventory.module.ts
@@ -1,10 +1,12 @@
+import { AgGridModule } from '@ag-grid-community/angular';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
+import { InventoryControlsComponent } from './inventory-controls/inventory-controls.component';
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [ProductListComponent],
-  imports: [InventoryRoutingModule, SharedModule],
+  declarations: [ProductListComponent, InventoryControlsComponent],
+  imports: [InventoryRoutingModule, SharedModule, AgGridModule.withComponents([])],
 })
 export class InventoryModule {}
